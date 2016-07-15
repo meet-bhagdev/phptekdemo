47aa6e5b7abeba72c28f30320bdf1535d7c0197b
Meet Bhagdev <mebha@microsoft.com>
2016-07-15 14:51:00 -0700

    Create test.py

---
+++ test.py
@@ -0,0 +1,12 @@
+import pyodbc
+server = 'tcp:csucla2015.database.windows.net'
+database = 'AdventureWorks'
+username = 'meet_bhagde'
+password = 'avengersA1'
+cnxn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+cursor = cnxn.cursor()
+cursor.execute("SELECT @@version;")
+row = cursor.fetchone()
+while row:
+    print row
+    row = cursor.fetchone()
